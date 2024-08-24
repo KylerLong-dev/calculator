4218f1e4fb5774e330c0ccfc189d012cbc505815
Kyler Long <kylerlong24@gmail.com>
2024-08-24 18:11:12 -0400

    add js function for operations

---
+++ script.js
@@ -0,0 +1,23 @@
+const firstNumber = "";
+const secondNumber = "";
+const operator = "";
+
+const toAdd = function (numOne, numTwo) {
+    return numOne + numTwo; 
+}
+
+const toSubtract = function (numOne, numTwo) {
+    return numOne - numTwo
+}
+
+const toMultiply = function (numOne, numTwo) {
+    return numOne * numTwo;
+}
+
+const toDivide = function (numOne, numTwo) {
+    return numOne / numTwo; 
+}
+
+const operate = function () {
+    
+}
