04c71d0759acc6eaf38bd1ad39a53ff97b95b342
Kyler Long <kylerlong24@gmail.com>
2024-08-24 22:42:55 -0400

    add operate function with callbacks to operator functions using num1, num2, and operator as paramters

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 
-const toAdd = function (numOne, numTwo) {
-    return numOne + numTwo; 
+let operator = "";
+let firstNumber = "";
+let secondNumber = "";
+let displayValue = "";
+
+const numbers = document.querySelector(".numbers");
+
+const toAdd = function (a, b) {
+    return a + b; 
 }
 
-const toSubtract = function (numOne, numTwo) {
-    return numOne - numTwo
+const toSubtract = function (a, b) {
+    return a - b;
 }
 
-const toMultiply = function (numOne, numTwo) {
-    return numOne * numTwo;
+const toMultiply = function (a, b) {
+    return a * b;
 }
 
-const toDivide = function (numOne, numTwo) {
-    return numOne / numTwo; 
+const toDivide = function (a, b) {
+    if (b === 0) {
+        return "ERROR: Division by 0";
+    }
+    return a / b; 
 }
 
-const operate = function () {
-
+const operate = function (num1, operator, num2) {
+    if (operator === "+") {
+        return toAdd(num1, num2);
+    }
+    else if (operator === "-") {
+        return toSubtract (num1, num2);
+    }
+    else if (operator === "*") {
+        return toMultiply(num1, num2);
+    }
+    else if (operator === "/") {
+        return toDivide(num1, num2);
+    }
+    else {
+        return "ERROR: Invalid Operator.";
+    }
 }
+
+console.log(operate(10, "+", 10));
